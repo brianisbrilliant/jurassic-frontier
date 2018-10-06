5f76a6b5ebfd8578c2503cfcfcf24803d6c487b4
Brian Foster <brianisbrilliant@users.noreply.github.com>
2018-10-06 13:08:16 -0500

    Added placeholder programmer art.
    Also set up the first scene to test the PlayerController.

---
+++ Jurassic Unity Project/Assets/GameMgr.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMgr : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("<color=blue>Game Manager has loaded.</color>");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
